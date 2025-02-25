a6204be0ba695b12acc4c0dcade88d934d924dc7
rywls1110 <rywls1110@naver.com>
2025-02-25 17:29:56 +0900

    Heap

---
+++ 자료구조/Heap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Input : 17 15 10 10 7 6
+        7
+    15      10
+  6   10  7   
+
+- Heap은 "반정렬"
+- 넣을 때 "반정렬" 되는가
+- 뺄 때 "정렬: 되었는가
+
+*/
+auto main() -> int
+{
+    
+    
+    
+    
+    return 0;
+}
