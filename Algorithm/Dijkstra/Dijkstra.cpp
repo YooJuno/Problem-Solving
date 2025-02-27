f0087c81ee0fb6d36d21e6c75444c6b124829355
rywls1110 <rywls1110@naver.com>
2025-02-27 11:08:18 +0900

    다익스트라 정복

--- Algorithm/Dijkstra/Dijkstra.cpp
+++ Algorithm/Dijkstra/Dijkstra.cpp
@@ -4,76 +4,82 @@
 
 using namespace std;
 
-// 무한대를 나타내는 값
-const int INF = 21e8;
+vector<pair<int, int>> graph[100001];
+int dist[100001];
+int N;
+int M;
 
-// 다익스트라 알고리즘 구현
-void dijkstra(int start, vector<vector<pair<int, int>>> &graph, vector<int> &dist)
+void Dijkstra(int src)
 {
-    // 우선순위 큐 (최소 힙) 사용
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    
-    // 시작 노드의 최단 거리를 0으로 설정하고 큐에 삽입
-    dist[start] = 0;
-    pq.push({0, start});
-    
-    while (!pq.empty())
+    pq.push({0, src});
+    dist[src] = 0;
+
+    while(!pq.empty())
     {
-        // 현재 노드 정보 가져오기
         int cost = pq.top().first;
         int u = pq.top().second;
         pq.pop();
-        
-        // 기존 거리보다 크면 무시
-        if (cost > dist[u])
+
+        // 같아도 안됨. u 이전까지의 cost가 0일 수도 있어서
+        if(dist[u] < cost)
         {
             continue;
         }
-        
-        // 인접 노드 확인
-        for (auto &neighbor : graph[u])
+
+        for(auto neighbor : graph[u])
         {
-            int v = neighbor.first;
-            int weight = neighbor.second;
-            
-            // 더 짧은 경로 발견 시 업데이트
-            if (dist[u] + weight < dist[v])
+            int weight = neighbor.first;
+            int v = neighbor.second;
+
+            if(dist[v] > dist[u] + weight)
             {
                 dist[v] = dist[u] + weight;
-                pq.push({dist[v], v});
             }
+
+            pq.push({dist[v], v});
         }
     }
+
 }
 
 int main()
 {
-    int V, E, start;
-    cin >> V >> E >> start;
-    
-    vector<vector<pair<int, int>>> graph(V);
-    vector<int> dist(V, INF);
-    
-    for (int i = 0; i < E; i++)
+    int src;
+    cin >> N >> M >> src;
+
+    for(int i = 0; i < M; i++)
     {
-        int u, v, w;
-        cin >> u >> v >> w;
-        graph[u].push_back({v, w});
+        int u, v, cost;
+        cin >> u >> v >> cost;
+        graph[u].push_back({cost, v});
     }
-    
-    dijkstra(start, graph, dist);
-    
-    for (int i = 0; i < V; i++)
+
+    for(int i = 1; i <= N; i++)
     {
-        if (dist[i] == INF)
-        {
-            cout << "INF" << endl;
-        }
-        else
-        {
-            cout << dist[i] << endl;
-        }
+        dist[i] = 21e8;
     }
+
+    Dijkstra(src);
+
+    cout << "src : " << src << endl;
+    for(int i = 1; i <= N; i++)
+    {
+        cout << "[" << src << " -> " << i << "] : " << dist[i] << endl;
+    }
+    cout << endl;   
     
     return 0;
 }
+
+/*
+6 7 1
+1 2 4
+1 3 2
+2 4 4
+2 5 5
+3 5 4
+4 6 1
+5 6 2
+
+*/
