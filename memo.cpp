ac595b56b654fe9984fac7db7833291af9eecb47
유준호 <juno980220@naver.com>
2025-03-05 15:28:37 +0900

    ps

--- memo.cpp
+++ memo.cpp
@@ -1,80 +1,46 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+struct Pos
+{
+    int y;
+    int x;
+};
+
+struct cmd
+{
+    bool operator()(pair<int, Pos> a, pair<int, Pos> b)
+    {
+        return a.first > b.first;
+    }
+};
+
 int N;
-int parent[1000];
-bool graph[1000][1000];
-
-int find(int x)
-{
-    if(parent[x] == x)
-    {
-        return x;
-    }
-
-    return parent[x] = find(parent[x]);
-}
-
-int unite(int a, int b)
-{
-    int rootA = find(a);
-    int rootB = find(b);
-    printf("%d %d\n", rootA, rootB);
-    if(rootA == rootB)
-    {
-        return true;
-    }
-
-    parent[rootB] = rootA;
-    return false;
-}
+int M;
 
 int main()
 {
-    cin >> N;
-    for (int i = 0; i < N; i++)
+    priority_queue<pair<int, Pos>, vector<pair<int, Pos>>, cmd> pq;
+    cin >> N >> M;
+    for (int i = 0; i < N + M; i++)
     {
-        parent[i] = i;
-    }
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
+        int B, Y, X;
+        cin >> B;
+        if(B == 0)
         {
-            cin >> graph[i][j];
+            pq.top().second.y, pq.top().second.x;
+            pq.pop();
+        }
+        else
+        {
+            cin >> Y >> X;
+            pq.push({B, {Y, X}});
         }
     }
 
-    cout << "parent : ";
-    for (int i = 0; i < N; i++)
-    {
-        cout << parent[i] << ' ';
-    }
-    cout << endl;
-
-    for (int i = 0; i < N-1; i++)
-    {
-        for (int j = i+1; j < N; j++)
-        {
-            if(graph[i][j] == true)
-            {
-                printf("i:%d j:%d => ", i, j);
-                unite(i, j);
-                cout << "parent : ";
-                for (int k = 0; k < N; k++)
-                {
-                    cout << parent[k] << ' ';
-                }
-                cout << endl;
-                // if(unite(i, j))
-                // {
-                //     cout << "WARNING\n";
-                //     return 0;
-                // }
-            }
-        }   
-    }
         
-    // cout << "STABLE\n";
+
     return 0;
 }
