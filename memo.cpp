6809e26ba8008e25351d46a68a8cde129c0cfb90
유준호 <juno980220@naver.com>
2025-03-13 08:53:19 +0900

    ps

--- memo.cpp
+++ memo.cpp
@@ -1,46 +1,14 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-struct Pos
-{
-    int y;
-    int x;
-};
 
-struct cmd
-{
-    bool operator()(pair<int, Pos> a, pair<int, Pos> b)
-    {
-        return a.first > b.first;
-    }
-};
 
-int N;
-int M;
 
 int main()
 {
-    priority_queue<pair<int, Pos>, vector<pair<int, Pos>>, cmd> pq;
-    cin >> N >> M;
-    for (int i = 0; i < N + M; i++)
-    {
-        int B, Y, X;
-        cin >> B;
-        if(B == 0)
-        {
-            pq.top().second.y, pq.top().second.x;
-            pq.pop();
-        }
-        else
-        {
-            cin >> Y >> X;
-            pq.push({B, {Y, X}});
-        }
-    }
-
-        
+    
 
     return 0;
 }
